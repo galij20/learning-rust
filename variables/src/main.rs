7747f7b15aaa29d1fca15c8e3ac58b351f5aac66
galij20 <196127646+galij20@users.noreply.github.com>
2025-06-04 23:22:20 +0545

    Add constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main(){
-    let x = 5;
+    //mutability
+    let mut x = 5;
     println!("The day of learning is {x}.");
     x = 4;
     println!("The day of learning is {x}.");
+
+    //constants
+    const LEARNING_OR_NOT: bool = true;
+    println!("Am I currently learning or not?");
+    println!("{LEARNING_OR_NOT}");
 }
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/bucho/git/learning-rust/variables/target/debug/variables: /home/bucho/git/learning-rust/variables/src/main.rs
