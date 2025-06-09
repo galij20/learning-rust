b6cbec241558ee75dd1b8b004d925c5d09538592
galij20 <196127646+galij20@users.noreply.github.com>
2025-06-09 21:55:34 +0545

    Add slices

---
+++ slices/target/debug/deps/slices-72d6cb4dc4034f17.d
@@ -0,0 +1,9 @@
+/home/bucho/git/learning-rust/slices/target/debug/deps/libslices-72d6cb4dc4034f17.rmeta: src/main.rs Cargo.toml
+
+/home/bucho/git/learning-rust/slices/target/debug/deps/slices-72d6cb4dc4034f17.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ slices/target/debug/deps/slices-d7e73599979c3bc1.d
@@ -0,0 +1,9 @@
+/home/bucho/git/learning-rust/slices/target/debug/deps/libslices-d7e73599979c3bc1.rmeta: src/main.rs Cargo.toml
+
+/home/bucho/git/learning-rust/slices/target/debug/deps/slices-d7e73599979c3bc1.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
