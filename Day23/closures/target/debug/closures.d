f32b0938ed1d4ee8cd773529798cf16c70244dcc
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-06-23 20:47:06 +0545

    Add closures/

---
+++ Day23/closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/home/bucho/git/learning-rust/Day23/closures/target/debug/closures: /home/bucho/git/learning-rust/Day23/closures/src/main.rs
---
+++ Day23/closures/target/debug/deps/closures-f76d3cad9876f36d.d
@@ -0,0 +1,9 @@
+/home/bucho/git/learning-rust/Day23/closures/target/debug/deps/libclosures-f76d3cad9876f36d.rmeta: src/main.rs Cargo.toml
+
+/home/bucho/git/learning-rust/Day23/closures/target/debug/deps/closures-f76d3cad9876f36d.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
