6c8cbd84d93a101786ef458862d70560c1111271
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-06-24 17:41:12 +0545

    Changes

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -49,4 +49,17 @@ fn main() {
         "The user with preference {:?} gets {:?}",
         user_pref2, giveaway2
     );
+
+    let example_closure = |x| x;
+
+    println!("{}", example_closure(String::from("It's LSPPDay23.")));
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
 }
