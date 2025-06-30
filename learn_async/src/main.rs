fddf45ff96b3d0f564a887a1fba463a6a9449895
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-06-30 23:14:49 +0545

    Multiple url

--- learn_async/src/main.rs
+++ learn_async/src/main.rs
@@ -1,22 +1,32 @@
 extern crate trpl; // required for mdbook test
 
-use trpl::Html;
+use trpl::{Either, Html};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            _none => println!("{url} had no title"),
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) =
+            match trpl::race(title_fut_1, title_fut_2).await {
+                Either::Left(left) => left,
+                Either::Right(right) => right,
+            };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("Its page title is: '{title}'"),
+            _none => println!("Its title could not be parsed."),
         }
     })
 }
 
-async fn page_title(url: &str) -> Option<String> {
-    let response_text = trpl::get(url).await.text().await;
-    Html::parse(&response_text)
+async fn page_title(url: &str) -> (&str, Option<String>) {
+    let text = trpl::get(url).await.text().await;
+    let title = Html::parse(&text)
         .select_first("title")
-        .map(|title_element| title_element.inner_html())
+        .map(|title| title.inner_html());
+    (url, title)
 }
