432da3f57e82556b37846e062c1d4e994cd26861
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-06-30 22:33:03 +0545

    Add async to single url processing

---
+++ learn_async/src/main.rs
@@ -0,0 +1,22 @@
+extern crate trpl; // required for mdbook test
+
+use trpl::Html;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::run(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            _none => println!("{url} had no title"),
+        }
+    })
+}
+
+async fn page_title(url: &str) -> Option<String> {
+    let response_text = trpl::get(url).await.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title_element| title_element.inner_html())
+}
