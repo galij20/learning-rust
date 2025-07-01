7e73291a049399c71f338a8198f852c147d9b2a8
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-07-01 22:45:08 +0545

    Add message passing

--- concurrency_async/src/main.rs
+++ concurrency_async/src/main.rs
@@ -1,21 +1,46 @@
-extern crate trpl;
+extern crate trpl; // required for mdbook test
 
 use std::time::Duration;
 
 fn main() {
     trpl::run(async {
-        let handle = trpl::spawn_task(async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        });
+        };
 
-        for i in 1..5 {
-            println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
 
-        handle.await.unwrap();
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
     });
 }
