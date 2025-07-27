ad223dd6d644f43812bb5d587ef0c6fd94b30b5c
Galij Sunuwar <galijsunuwar920@gmail.com>
2025-07-27 22:37:29 +0545

    Make multithreaded with execute method

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -5,21 +5,24 @@ use std::{
     thread,
     time::Duration,
 };
-
+use hello::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    
+
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
@@ -27,8 +30,8 @@ fn handle_connection(mut stream: TcpStream) {
             ("HTTP/1.1 200 OK", "hello.html")
         }
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    }; 
-    
+    };
+
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
@@ -36,5 +39,4 @@ fn handle_connection(mut stream: TcpStream) {
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-    
 }
